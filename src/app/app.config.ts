85914a8a5938873f3665ffe711b1c2e4e8f0015e
yadhukrishnan <yadhu623871@gmail.com>
2026-09-05 13:13:38 +0530

    fix: provide zoneless change detection in appConfig

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
- provideHttpClient()
-    
-  ]
+    provideZonelessChangeDetection(),
+    provideHttpClient(),
+  ],
 };
-
-
